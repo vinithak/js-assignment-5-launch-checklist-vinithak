a65a63ce53ef0df09e94e5822e4763e27e6eda98
VInitha <vinivinitha@gmail.com>
2023-08-03 13:07:07 -0500

    Commented require

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,24 +1,26 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+//require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     const missionTarget = document.getElementById("missionTarget");
-    missionTarget.innerhtml = 
-
-                `<h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: ${name}</li>
-                    <li>Diameter: ${diameter}</li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: ${distance}</li>
-                    <li>Number of Moons: ${moons}</li>
-                </ol>
-                <img src="imageUrl">`;
+    console.log(missionTarget);
+    console.log(missionTarget.innerhtml);
+    missionTarget.style.color = "green";
+    // missionTarget.innerhtml = 
+    //            `<h2>Mission Destination</h2>
+    //             <ol>
+    //                 <li>Name: ${name}</li>
+    //                 <li>Diameter: ${diameter}</li>
+    //                 <li>Star: ${star}</li>
+    //                 <li>Distance from Earth: ${distance}</li>
+    //                 <li>Number of Moons: ${moons}</li>
+    //             </ol>
+    //             <img src=${imageUrl}>`;
    
 }
 
 function validateInput(testInput) {
-    if(testInput = ""){
+    if(!testInput){
         return "Empty";
     }
     else if(isNaN(testInput)){
@@ -33,12 +35,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
 
         
-        const launchStatus = document.getElementById("launchStatus");
+        const launchStatus = document.querySelector("launchStatus"); //document.getElementById("launchStatus");
         const pilotStatus = document.getElementById("pilotStatus");
         const copilotStatus = document.getElementById("copilotStatus");
         const fuelStatus = document.getElementById("fuelStatus");
         const cargoStatus = document.getElementById("cargoStatus");
-
         pilotStatus.innerhtml = 'Pilot ${pilot} Ready';
         copilotStatus.innerhtml = 'Co-pilot ${copilot} Ready';
 
@@ -68,26 +69,17 @@ async function myFetch() {
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
        return response.json() });
-    //console.log(planetsReturned);
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
-    let index = (Math.floor((Math.random() * planets.length)));
+    let index = Math.floor((Math.random() * planets.length));
     return planets[index];
 }
 
-// module.exports.addDestinationInfo = addDestinationInfo;
-// module.exports.validateInput = validateInput;
-// module.exports.formSubmission = formSubmission;
-// module.exports.pickPlanet = pickPlanet; 
-// module.exports.myFetch = myFetch;
 
-
-module.exports = {
-    addDestinationInfo: addDestinationInfo,
-    validateInput: validateInput,
-    formSubmission: formSubmission,
-    pickPlanet: pickPlanet,
-    myFetch: myFetch
- }
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet; 
+module.exports.myFetch = myFetch;
+module.exports.addDestinationInfo = addDestinationInfo;
