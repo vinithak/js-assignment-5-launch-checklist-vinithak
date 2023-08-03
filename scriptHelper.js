b56feb752f6a15b1ef76acce67594b1b329a3c6c
VInitha <vinivinitha@gmail.com>
2023-08-03 13:46:11 -0500

    Fixed issues - innerHTML and empty string

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,21 +1,18 @@
 // Write your helper functions here!
-//require('isomorphic-fetch');
+require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     const missionTarget = document.getElementById("missionTarget");
-    console.log(missionTarget);
-    console.log(missionTarget.innerhtml);
-    missionTarget.style.color = "green";
-    // missionTarget.innerhtml = 
-    //            `<h2>Mission Destination</h2>
-    //             <ol>
-    //                 <li>Name: ${name}</li>
-    //                 <li>Diameter: ${diameter}</li>
-    //                 <li>Star: ${star}</li>
-    //                 <li>Distance from Earth: ${distance}</li>
-    //                 <li>Number of Moons: ${moons}</li>
-    //             </ol>
-    //             <img src=${imageUrl}>`;
+    missionTarget.innerHTML = 
+               `<h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
+                </ol>
+                <img src=${imageUrl}>`;
    
 }
 
@@ -40,25 +37,25 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         const copilotStatus = document.getElementById("copilotStatus");
         const fuelStatus = document.getElementById("fuelStatus");
         const cargoStatus = document.getElementById("cargoStatus");
-        pilotStatus.innerhtml = 'Pilot ${pilot} Ready';
-        copilotStatus.innerhtml = 'Co-pilot ${copilot} Ready';
+        pilotStatus.innerHTML = `Pilot ${pilot} Ready`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`;
 
         if(fuelLevel < 10000){
-            list.visible = true;
-            fuelStatus.innerhtml = "Fuel Level Low - There is not enough fuel for the journey";
-            launchStatus.innerhtml = "Shuttle not ready for launch";
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel Level Low - There is not enough fuel for the journey";
+            launchStatus.innerHTML = "Shuttle not ready for launch";
             launchStatus.style.color = "red";
         }
         else if(cargoLevel > 10000){
-            list.visible = true;
-            cargoStatus.innerhtml = "There is too much mass for the shuttle to take off";
-            launchStatus.innerhtml = "Shuttle not ready for launch";
-            launchStatus.style.color = rgb(199, 37, 78);
+            list.style.visibility = "visible";
+            cargoStatus.innerHTML = "There is too much mass for the shuttle to take off";
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "rgb(199, 37, 78)";
         }
         else{
-            list.visible = false;
-            launchStatus.innerhtml = "Shuttle is ready for launch";
-            launchStatus.style.color = rgb(65, 159, 106);           
+            list.style.visibility = "hidden";
+            launchStatus.innerHTML = "Shuttle is ready for launch";
+            launchStatus.style.color = "rgb(65, 159, 106)";           
         }
 
 
